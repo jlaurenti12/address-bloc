a234fe6989ddfe30cf443acaaa99d9bddffb9846
Jacob <jake.laurenti@gmail.com>
2017-11-09 22:13:39 -0500

    Assignment 4 Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,17 +7,28 @@ require_relative 'entry'
       @entries = []
     end
 
-  def add_entry(name, phone_number, email)
-    # #9
-    index = 0
-    entries.each do |entry|
-    # #10
-      if name < entry.name
-        break
+    def add_entry(name, phone_number, email)
+      # #9
+      index = 0
+      entries.each do |entry|
+      # #10
+        if name < entry.name
+          break
+        end
+        index+= 1
       end
-      index+= 1
+      # #11
+      entries.insert(index, Entry.new(name, phone_number, email))
     end
-    # #11
-    entries.insert(index, Entry.new(name, phone_number, email))
-  end
+
+    def remove_entry(name, phone_number, email)
+      selectedEntry = ""
+      @entries.each do |entry|
+        if name == entry.name && phone_number == entry.phone_number && email == entry.email
+          selectedEntry = entry
+        end
+      end
+
+      @entries.delete(selectedEntry)
+    end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,17 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
+
+  describe "#remove_entry" do
+    it "removes the selected entry from the address book" do
+      book = AddressBook.new
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Jacob Laurenti', '010.012.1816', 'jacob@laurenti.com')
+
+      size = book.entries.size
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(1)
+      expect(book.entries.first.name).to eq("Jacob Laurenti")
+    end
+  end
 end
